b9654224c4567a335842f70c024c565cd21ebfa3
Rahulgaire <gairerahul334@gmail.com>
2025-11-21 01:19:54 +0530

    Updations

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,15 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://snapmart-ayll.onrender.com",
+      "https://snapmart-ayll.onrender.com", // only root, nothing else
+      "http://localhost:5173"
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 // Routes
 app.use("/api/cart", cartRoutes);
